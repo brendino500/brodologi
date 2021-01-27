af383696437778eb7f060c1c093f9695022bf5d5
brendino500 <brenda.ty@live.com>
2021-01-27 15:47:10 +0000

    React set up

---
+++ frontend/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Brødologi</h1>
+}
+
+export default App
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
+const logger = require('./lib/logger')
 const router = require('./config/router')
+const errorHandler = require('./lib/errorHandler')
 const { PORT, dbURI } = require('./config/environment')
 
 mongoose.connect(
@@ -17,6 +18,22 @@ mongoose.connect(
   }
 )
 
+app.use(express.static(`${__dirname}/frontend/build`))
+
+app.use(express.json())
+
+app.use(logger)
+
 app.use('/api', router)
 
+app.use('/*', (_, res) =>
+  res.sendFile(`${__dirname}/frontend/build/index.html`)
+)
+
+app.use(errorHandler)
+
 app.listen(PORT, () => console.log(`Up and running on port ${PORT}`))
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!')
+})
